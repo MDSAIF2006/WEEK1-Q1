657f6208aeab55b946296c707fa9aa8945b256b8
MDSAIF2006 <24331a05i1@mvgrce.edu.in>
2025-12-16 18:24:22 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,8 @@
+num1=int(input("enter number 1 :"))
+print("value of number 1",num1)
+char='a'
+print("character is :",char)
+float=float(input("enter number 2:"))
+print("float value",float)
+str="Saif"
+print("string is :",str)
